80b39aa894a24a01b3d2bd3cd19bb78b8459b6c3
Andreza Freitas <andreza1freitas@gmail.com>
2022-07-18 18:18:45 -0300

    Obtendo um usuário por id

--- src/main/java/com/andrezafreitas/workshopmongo/services/UserService.java
+++ src/main/java/com/andrezafreitas/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.andrezafreitas.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andrezafreitas.workshopmongo.domain.User;
 import com.andrezafreitas.workshopmongo.repository.UserRepository;
+import com.andrezafreitas.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -15,7 +17,11 @@ public class UserService {
 	private UserRepository repo;
 	
 	public List<User> findAll() {
-		return repo.findAll();
-		
+		return repo.findAll();		
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
