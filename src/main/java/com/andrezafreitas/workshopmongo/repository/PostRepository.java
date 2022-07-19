22e9f3585119480ad915e27904ae53a8ba890e17
Andreza Freitas <andreza1freitas@gmail.com>
2022-07-19 16:48:57 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/andrezafreitas/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.andrezafreitas.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.andrezafreitas.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
